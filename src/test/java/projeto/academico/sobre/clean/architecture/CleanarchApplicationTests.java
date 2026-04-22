7ad8d7d5dcfb0a9f3b6022866b0c09d6f9f77f86
Elias P. Pereira <eliaspinheiropereiraa@gmail.com>
2026-04-22 15:13:42 -0300

    iniciando o projeto cleanarch

---
+++ src/test/java/projeto/academico/sobre/clean/architecture/CleanarchApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.academico.sobre.clean.architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanarchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
